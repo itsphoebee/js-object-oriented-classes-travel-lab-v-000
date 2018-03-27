71be2295fcd167eff1431745f0eb1134665fbc5d
Phoebe Lau <phoebelau7@gmail.com>
2018-03-27 18:11:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+class Driver {
 
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+}
