ce75bc23eeda3f50cb7d66773b4d51cd20fbe919
Phoebe Lau <phoebelau7@gmail.com>
2018-03-27 18:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 class Driver {
-
+  constructor(name, date){
+    this.name = name
+    this.date = date
+  }
 }
+
+Driver.prototype.startDate
 
 class Route {
   constructor(beginningLocation, endingLocation){
