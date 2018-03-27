0c7f9eb5746a7defdb105a3340079aca46ea35b9
Phoebe Lau <phoebelau7@gmail.com>
2018-03-27 18:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ Route.prototype.blocksTravelled = function(beginningLocation, endingLocation){
     return eastWest.indexOf(avenue)
   }
 
-  const horizontalDistance = distanceAsIndex(Object.values(beginningLocation)[0]) - Object.values(endingLocation)[0]
+  const horizontalDistance = distanceAsIndex(Object.values(beginningLocation)[0]) - distanceAsIndex(Object.values(endingLocation)[0])
   const verticalDistance = Object.values(beginningLocation)[1] - Object.values(endingLocation)[1]
-  
+
   return totalDistance = horizontalDistance + verticalDistance;
 }
 
